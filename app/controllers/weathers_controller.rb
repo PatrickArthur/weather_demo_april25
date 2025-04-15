bb126df8a2b35f1fa0313c3d432009c07772c42d
patrick <patrick@arthurfamily.ws>
2025-04-15 00:25:20 -0400

    cleanup

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -7,6 +7,12 @@ class WeathersController < ApplicationController
     @weather = Weather.new
   end
 
+  def edit
+    @weather = Weather.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+     redirect_to weathers_path, notice: "Weather not found."
+  end
+
   def create
     @weather = Weather.new(weather_params)
 
@@ -15,6 +21,27 @@ class WeathersController < ApplicationController
     else
       flash.now[:alert] = 'Weather creation failed.'
       render :new
+    end
+  end
+
+  def update
+    @weather = Weather.find(params[:id])
+
+    if @weather.update(weather_params)
+      redirect_to weathers_path, notice: 'Weather data updated successfully.'
+    else
+      flash.now[:alert] = 'Weather update failed.'
+      render :edit
+    end
+  end
+
+  def destroy
+    @weather = Weather.find(params[:id])
+
+    if @weather.destroy
+      redirect_to weathers_path, notice: 'Weather data deleted successfully.'
+    else
+      redirect_to weathers_path, alert: 'Weather deletion failed.'
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :weathers, only: [:index, :create, :new]
+  resources :weathers, only: [:index, :create, :new, :destroy, :edit, :update]
 
   # Defines the root path route ("/")
   root "weathers#index"
